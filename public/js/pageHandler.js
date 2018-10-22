3584f7fe664b5f4b28ecd00905839971426eb65e
Kishor Subedi <subedik@subedik-macbookpro.roam.corp.google.com>
2018-10-22 16:41:53 -0700

    Forgot Password

--- public/js/pageHandler.js
+++ public/js/pageHandler.js
@@ -21,7 +21,7 @@ firebase.auth().onAuthStateChanged(function(user){
 })
 
 
-function linkGoogleAccount()
+function linkGoogleAccount() //Calls the firebase link google account.
 {
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().currentUser.linkWithPopup(provider).then(function(result) {
@@ -37,8 +37,8 @@ function linkGoogleAccount()
     });
 }
 
-function signOut(){
-    console.log("Llama el metodo");
+function signOut(){ //Calls the firebase signout method
+    console.log("Signed Out successfully");
     firebase.auth().signOut().then(function() {
         document.location.href = "../index.html";
         // Sign-out successful.
