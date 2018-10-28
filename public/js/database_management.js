0984ff9e443e405a93fd21f90873b3fb8cdec8cf
kristalys <kristalys@google.com>
2018-10-27 21:48:26 -0700

    Delete works

--- public/js/database_management.js
+++ public/js/database_management.js
@@ -83,7 +83,7 @@ function deleteChallenge(challenge, div, editButtons, deleteButtons){
                             break;
                         }
                     }
-                    if (j != -1) {
+                    if (j !== -1) {
                         ownChallenges.splice(j, 1);
 
                         t.update(users.doc(user), {ownChallenges: ownChallenges});
@@ -100,7 +100,7 @@ function deleteChallenge(challenge, div, editButtons, deleteButtons){
                     }
 
                 });
-        }).then(result => {
+        }).then(function () {
             console.log('Transaction success!');
         }).catch(err => {
             console.log('Transaction failure:', err);
@@ -108,33 +108,6 @@ function deleteChallenge(challenge, div, editButtons, deleteButtons){
     }
 }
 
-/**
- * Creates a group
- * @param groupName name of the group
- * @param groupOwnerUsername the ownser UID
- */
-function createGroupQUERY(groupName, groupOwnerUsername) {
-    var query = groups.add({
-        groupName: groupName,
-        groupOwner: users.doc(groupOwnerUsername),
-        members: []
-    }).then(function (e) {
-        // This transaction makes possible the update in the list of the user.
-        var transaction = firestore.runTransaction(t => {
-            return t.get(users.doc(groupOwnerUsername))
-                .then(doc => {
-                    const ownGroupArray = doc.data().ownGroup;
-                    ownGroupArray.push(groups.doc(e.id));
-                    t.update(users.doc(groupOwnerUsername), {ownGroup: ownGroupArray});
-                });
-        }).then(result => {
-            console.log('Transaction success!');
-        }).catch(err => {
-            console.log('Transaction failure:', err);
-        });
-        addToGroup(groupOwnerUsername, e.id)
-    });
-}
 
 /**
  * Create a challenge
@@ -232,76 +205,6 @@ function createChallengeQUERY(URL, songname, artist, genre, hint, isPublic, opti
 }
 
 /**
- * Get user by Username
- * @param username the user username
- */
-function getUserbyUsernameQUERY(username) {
-    var query = users.where("username", "==", username);
-    query.get().then(function (results) {
-        if (results.empty) {
-            console.log("No documents found!");
-        } else {
-            var user;
-            // go through all results
-            results.forEach(function (doc) {
-                //user = doc.data().id;
-                console.log("Document data:", doc.data());
-            });
-
-
-            // or if you only want the first result you can also do something like this:
-            //console.log("Document data:", results.docs[0].data());
-        }
-    }).catch(function (error) {
-        console.log("Error getting documents:", error);
-    });
-
-}
-
-/**
- * Get user by Email
- * @param email the user email
- */
-function getUserbyEmailQUERY(email) {
-    var query = users.where("email", "==", email);
-    query.get().then(function (results) {
-        if (results.empty) {
-            console.log("No documents found!");
-        } else {
-            // go through all results
-            results.forEach(function (doc) {
-                console.log("Document data:", doc.data());
-            });
-
-            // or if you only want the first result you can also do something like this:
-            //console.log("Document data:", results.docs[0].data());
-        }
-    }).catch(function (error) {
-        console.log("Error getting documents:", error);
-    });
-}
-
-/**
- * Add user to contactlist
- * @param username user UID
- * @param contactUsername contact UID
- */
-function addContact(username, contactUsername) {
-    var transaction = firestore.runTransaction(t => {
-        return t.get(users.doc(username))
-            .then(doc => {
-                const contactArray = doc.data().contactList;
-                contactArray.push(users.doc(contactUsername));
-                t.update(users.doc(username), {contactList: contactArray});
-            });
-    }).then(result => {
-        console.log('Transaction success!');
-    }).catch(err => {
-        console.log('Transaction failure:', err);
-    });
-}
-
-/**
  * Add user to group
  * @param username user to add UID
  * @param groupID ID of the group
@@ -374,6 +277,103 @@ function getUserChallengesQUERY() {
 
 }
 
+/**
+ * Get user by Username
+ * @param username the user username
+ */
+function getUserbyUsernameQUERY(username) {
+    var query = users.where("username", "==", username);
+    query.get().then(function (results) {
+        if (results.empty) {
+            console.log("No documents found!");
+        } else {
+            var user;
+            // go through all results
+            results.forEach(function (doc) {
+                //user = doc.data().id;
+                console.log("Document data:", doc.data());
+            });
+
+
+            // or if you only want the first result you can also do something like this:
+            //console.log("Document data:", results.docs[0].data());
+        }
+    }).catch(function (error) {
+        console.log("Error getting documents:", error);
+    });
+
+}
+
+/**
+ * Get user by Email
+ * @param email the user email
+ */
+function getUserbyEmailQUERY(email) {
+    var query = users.where("email", "==", email);
+    query.get().then(function (results) {
+        if (results.empty) {
+            console.log("No documents found!");
+        } else {
+            // go through all results
+            results.forEach(function (doc) {
+                console.log("Document data:", doc.data());
+            });
+
+            // or if you only want the first result you can also do something like this:
+            //console.log("Document data:", results.docs[0].data());
+        }
+    }).catch(function (error) {
+        console.log("Error getting documents:", error);
+    });
+}
+
+/**
+ * Add user to contactlist
+ * @param username user UID
+ * @param contactUsername contact UID
+ */
+function addContact(username, contactUsername) {
+    var transaction = firestore.runTransaction(t => {
+        return t.get(users.doc(username))
+            .then(doc => {
+                const contactArray = doc.data().contactList;
+                contactArray.push(users.doc(contactUsername));
+                t.update(users.doc(username), {contactList: contactArray});
+            });
+    }).then(result => {
+        console.log('Transaction success!');
+    }).catch(err => {
+        console.log('Transaction failure:', err);
+    });
+}
+
+/**
+ * Creates a group
+ * @param groupName name of the group
+ * @param groupOwnerUsername the ownser UID
+ */
+function createGroupQUERY(groupName, groupOwnerUsername) {
+    var query = groups.add({
+        groupName: groupName,
+        groupOwner: users.doc(groupOwnerUsername),
+        members: []
+    }).then(function (e) {
+        // This transaction makes possible the update in the list of the user.
+        var transaction = firestore.runTransaction(t => {
+            return t.get(users.doc(groupOwnerUsername))
+                .then(doc => {
+                    const ownGroupArray = doc.data().ownGroup;
+                    ownGroupArray.push(groups.doc(e.id));
+                    t.update(users.doc(groupOwnerUsername), {ownGroup: ownGroupArray});
+                });
+        }).then(result => {
+            console.log('Transaction success!');
+        }).catch(err => {
+            console.log('Transaction failure:', err);
+        });
+        addToGroup(groupOwnerUsername, e.id)
+    });
+}
 
 function getUserGroupsQUERY(username) {
     var query = users.belongsToGroup;
