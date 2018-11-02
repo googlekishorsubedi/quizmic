911aec36c577a31da82c56fb5369aa348a0e0de5
Tabby <tabiac4@gmail.com>
2018-11-02 10:02:55 -0700

    changed profile page

---
+++ public/js/profile.js
@@ -0,0 +1,23 @@
+
+// document.getElementById('creategroupbut').addEventListener('click', function(){
+//   document.getElementById('creategroupbox').style.display = 'flex';
+// });
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function addfriendsbut() {
+    document.getElementById("friendUsername").style.display = "block";
+    document.getElementById("addfriendsubmit").style.display = "block";
+
+}
+function invitebut() {
+  document.getElementById("findfriend").style.display = "block";
+  document.getElementById("groupsearch").style.display = "block";
+  document.getElementById("invitesubmit").style.display = "block";
+
+}
