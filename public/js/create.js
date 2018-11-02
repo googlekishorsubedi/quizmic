75eb63a42039373453af11dacd908066ade48ee6
Tabby <tabiac4@gmail.com>
2018-11-01 18:36:08 -0700

    reloaded the page after delete a challenge

--- public/js/create.js
+++ public/js/create.js
@@ -82,6 +82,7 @@ function createButtonSections(challenge) {
     deleteButton.className = "deleteButton";
     deleteButton.onclick = function () {
              deleteChallenge(challenge, div, editButton, deleteButton);
+
          };
 
     challengeName.innerHTML = challenge.challengeName;
@@ -213,8 +214,10 @@ function deleteChallenge(challenge) {
                             j = i;
                             break;
                         }
+                        window.location.reload();
                     }
                     if (j !== -1) {
+
                         ownChallenges.splice(j, 1);
 
                         t.update(users.doc(user), {ownChallenges: ownChallenges});
@@ -231,11 +234,14 @@ function deleteChallenge(challenge) {
                     }
 
                 });
+
         }).then(function () {
+            window.location.reload();
             console.log('Transaction success!');
         }).catch(err => {
             console.log('Transaction failure:', err);
         });
+
     }
 }
 
@@ -387,5 +393,3 @@ function createChallengeQUERY(challengeName, URL, songname, artist, genre, hint,
 
     })
 }
-
-
