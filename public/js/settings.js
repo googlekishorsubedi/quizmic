65aea0b3cec6168f974bce1564093a9ed559abc2
Tabby <tabiac4@gmail.com>
2018-11-20 19:55:35 -0800

    changes to settings page and dashboard

---
+++ public/js/settings.js
@@ -0,0 +1,11 @@
+function openprofilepopup() {
+  document.getElementById("profilepicpopup").style.display = "block";
+}
+
+function closeprofilepopup() {
+    document.getElementById("profilepicpopup").style.display = "none";
+}
+
+function x() {
+  console.log("sd");
+}
